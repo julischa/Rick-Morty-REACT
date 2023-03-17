6347c80813ff07e32e5482615f07c34f5bbf6be0
julescwrt <julianeschawert@gmail.com>
2023-03-17 17:46:34 +0100

    implemented react typewriter effect

--- src/App.js
+++ src/App.js
@@ -4,45 +4,37 @@ import Cards from "./cards/Card";
 import { useEffect, useState } from "react";
 import Search from "./search/Search";
 import "./App.css";
-
+import Typewriter from "typewriter-effect";
 
 function App() {
-  let [pageNumber, setPageNumber] = useState(1);
-  let [search, setSearch ] = useState("")
-  let [fetchedData, setFetchedData] = useState({});
-  let { info, results } = fetchedData;
-  console.log(info)
-  let api = `https://rickandmortyapi.com/api/character?page=${pageNumber}&name=${search}`;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [search, setSearch] = useState("");
+  const [fetchedData, setFetchedData] = useState({});
+  const { info, results } = fetchedData;
+  console.log(info);
+  const api = `https://rickandmortyapi.com/api/character?page=${pageNumber}&name=${search}`;
 
-  //retrieve data from an API endpoint
-  //to update the component's state with the fetched data:
-  //((((async function runs in a non-blocking way, meaning that it doesn't stop or block the execution of other code while it's
-  //the rest of the code))))
   async function fetchCharacters() {
-    let data = await fetch(api).then((res) => res.json());
+    const data = await fetch(api).then((res) => res.json());
     setFetchedData(data);
   }
-
-  //the useEffect will run (as side effect) when the search or pageNumber changes
-  //"on render"
-    useEffect(() => {
+  // const text = new Typewriter("#typewriter", {
+  //   strings: ["Guess who just discovered a new element?!"],
+  //   autoStart: true,
+  // });
+  useEffect(() => {
     fetchCharacters();
-    }, [search, pageNumber]);
-  //should only run when the search or pageNumber change, 
-  //otherwise it will keep fetching the same data over and over again if they are not included as dependencies
+  }, [search, pageNumber]);
 
-//function sets the pagenumber to 1 when called
-//If info and next are defined, it takes the slice of the next property starting from 47th index and assign it to the variable nextPage
   const handleNextPage = () => {
     if (info && info.next === null) {
       setPageNumber(1);
     } else {
-      const nextPage = info.next.slice(47)
+      const nextPage = info.next.slice(47);
       setPageNumber(nextPage);
     }
   };
 
-  //If info and prev are defined, it takes the slice of the prev property starting from 47th index and assign it to the variable prevPage
   const handlePrevPage = () => {
     if (info && info.prev === null) {
       setPageNumber(info.pages);
@@ -52,40 +44,61 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    // new Typewriter("#typewriter", {
+    //   strings: ["Guess who just discovered a new element?!"],
+    //   autoStart: true,
+    // });
+  }, []);
+
   return (
     <div className="app">
       <div className="ux-app">
-      <h1 className="text-center">Guess who just discovered <br></br>a new element?!</h1>
-      <div className="container d-flex flex-wrap justify-content-center">
-      <button className="button" onClick={() => setPageNumber(1)}>main</button>
-      <button className ="button" onClick={handlePrevPage}>←</button>
-      <button className="button" onClick={handleNextPage}>→</button>
-      <Search setSearch={setSearch}
-          search={search} 
-          pageNumber={pageNumber} 
-          setPageNumber={setPageNumber}/>
+        {/* <h1 id="typewriter" className="text-center">
+          Guess who just discovered a new element?!
+        </h1> */}
+        <Typewriter
+          className="typo"
+          options={{
+            strings: ["Guess who just discovered a new element?!"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+        <div className="container d-flex flex-wrap justify-content-center">
+          <button className="button" onClick={() => setPageNumber(1)}>
+            main
+          </button>
+          <button className="button" onClick={handlePrevPage}>
+            ←
+          </button>
+          <button className="button" onClick={handleNextPage}>
+            →
+          </button>
+          <Search
+            setSearch={setSearch}
+            search={search}
+            pageNumber={pageNumber}
+            setPageNumber={setPageNumber}
+          />
         </div>
-        </div>
-        
+      </div>
+
       <div className="container d-flex justify-content-center">
-          <div className="col-8">
-            <div className="row">
-              {results &&
-  results.map((character) => {
-    return (
-      <Cards character={character} key={character.id} />
-    );
-  })}
+        <div className="col-8">
+          <div className="row">
+            {results &&
+              results.map((character) => {
+                return <Cards character={character} key={character.id} />;
+              })}
           </div>
           <div className="footer">
-  <h1>Copyright &copy; {new Date().getFullYear()} [adult swim]</h1>
-</div>
+            <h1>&copy; {new Date().getFullYear()} [ juli schawert ]</h1>
           </div>
+        </div>
       </div>
     </div>
-    
   );
 }
-
 
 export default App;
--- src/search/Search.js
+++ src/search/Search.js
@@ -1,34 +1,22 @@
-import React from 'react'
-    
+import React from "react";
+
 let Search = ({ setSearch, setPageNumber }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setSearch(e.target.value); 
+    setSearch(e.target.value);
     setPageNumber("");
-  }
+  };
   return (
     <form className="d-flex justify-content-center">
       <input
         onChange={handleSubmit}
-        placeholder="Search for" 
-        type="text" 
+        placeholder="Search for"
+        type="text"
         className=""
-      >
-      </input>
+      ></input>
       <button className="search-button">Search</button>
     </form>
-  )
-}
+  );
+};
 
 export default Search;
-
-// This is a functional component called Search that accepts two props, setSearch and setPageNumber.
-
-//     It defines a function handleSubmit which takes an event as an argument.
-//     Inside the function, it first prevents the default behavior of the event.
-//     It then uses the setSearch prop to set the value of the input field as the value of the search state.
-//     It also uses the setPageNumber prop to reset the pageNumber state to an empty string.
-//     It returns a JSX form with an input field and a button.
-//     The input field has an onChange event listener that calls the handleSubmit function when the input value is changed.
-//     The button have class of search-button
-//     The component is exported as default so that it can be imported and used in other parts of the application.
