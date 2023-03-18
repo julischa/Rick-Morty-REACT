a1679c603d2b9f6f2a234e4e660b53348a73e48e
julescwrt <julianeschawert@gmail.com>
2023-03-18 23:32:38 +0100

    implemented responsiveness throughout the app 📱

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap";
 import Cards from "./cards/Card";
 import { useEffect, useState } from "react";
 import Search from "./search/Search";
 import "./App.css";
 import Typewriter from "typewriter-effect";
+import Github from "./assets/github.png";
 
 function App() {
   const [pageNumber, setPageNumber] = useState(1);
   const [search, setSearch] = useState("");
   const [fetchedData, setFetchedData] = useState({});
   const { info, results } = fetchedData;
-  console.log(info);
+
   const api = `https://rickandmortyapi.com/api/character?page=${pageNumber}&name=${search}`;
 
   async function fetchCharacters() {
     const data = await fetch(api).then((res) => res.json());
     setFetchedData(data);
   }
-  // const text = new Typewriter("#typewriter", {
-  //   strings: ["Guess who just discovered a new element?!"],
-  //   autoStart: true,
-  // });
+
+  // If you want to filter through all characters, regardless of their pagination, you may find this code snippet helpful:
+
   useEffect(() => {
     fetchCharacters();
   }, [search, pageNumber]);
@@ -44,44 +43,37 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    // new Typewriter("#typewriter", {
-    //   strings: ["Guess who just discovered a new element?!"],
-    //   autoStart: true,
-    // });
-  }, []);
+  // By slicing this URL starting from the 47th character the code can extract the value "2"
+  // and update the pageNumber state variable accordingly.
 
   return (
     <div className="app">
-      <div className="ux-app">
-        {/* <h1 id="typewriter" className="text-center">
-          Guess who just discovered a new element?!
-        </h1> */}
+      <div className="ux-app text-center" id="ux-app">
+        Guess who just discovered
         <Typewriter
-          className="typo"
           options={{
-            strings: ["Guess who just discovered a new element?!"],
+            strings: ["a new element?!"],
             autoStart: true,
             loop: true,
           }}
         />
-        <div className="container d-flex flex-wrap justify-content-center">
-          <button className="button" onClick={() => setPageNumber(1)}>
-            main
-          </button>
-          <button className="button" onClick={handlePrevPage}>
-            ←
-          </button>
-          <button className="button" onClick={handleNextPage}>
-            →
-          </button>
-          <Search
-            setSearch={setSearch}
-            search={search}
-            pageNumber={pageNumber}
-            setPageNumber={setPageNumber}
-          />
-        </div>
+      </div>
+      <div className="container d-flex flex-wrap justify-content-center">
+        <button className="button" onClick={() => setPageNumber(1)}>
+          main
+        </button>
+        <button className="button" onClick={handlePrevPage}>
+          ←
+        </button>
+        <button className="button" onClick={handleNextPage}>
+          →
+        </button>
+        <Search
+          setSearch={setSearch}
+          search={search}
+          pageNumber={pageNumber}
+          setPageNumber={setPageNumber}
+        />
       </div>
 
       <div className="container d-flex justify-content-center">
@@ -92,8 +84,41 @@ function App() {
                 return <Cards character={character} key={character.id} />;
               })}
           </div>
-          <div className="footer">
-            <h1>&copy; {new Date().getFullYear()} [ juli schawert ]</h1>
+          <div
+            className="footer"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h1 style={{ display: "flex", alignItems: "center" }}>
+              <a
+                href="https://github.com/julischa"
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  src={Github}
+                  alt="Github"
+                  id="github"
+                  style={{
+                    width: "30px",
+                    marginRight: "10px",
+                    marginBottom: "7px",
+                    transition: "transform 0.5s ease-in-out",
+                  }}
+                />
+                <span style={{ marginRight: "10px" }}>
+                  {new Date().getFullYear()}
+                </span>
+                <span>[ juli schawert ]</span>
+              </a>
+            </h1>
           </div>
         </div>
       </div>
--- src/cards/Card.js
+++ src/cards/Card.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Card = ({character}) => {
-    return (
-        <div className="col-3">
-            <div className="">
-                <img src={character.image} alt="rick and morty character pic" className="img-fluid my-3" />
-                <p className="character-name d-flex justify-content-center">{character.name}</p>
-            </div>  
-        </div>
-    )
-}; 
+const Card = ({ character }) => {
+  return (
+    <div className="col-12 col-md-6 col-lg-4 col-xl-3">
+      <div className="">
+        <img
+          src={character.image}
+          alt="rick and morty character pic"
+          className="img-fluid my-3"
+        />
+        <p className="character-name d-flex justify-content-center">
+          {character.name}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
