044a8b515237964b314f38e45ef980d443de5eb5
julescwrt <julianeschawert@gmail.com>
2023-03-19 16:57:50 +0100

    Improved mobile responsiveness 📱

--- src/cards/Card.js
+++ src/cards/Card.js
@@ -3,15 +3,13 @@ import React from "react";
 const Card = ({ character }) => {
   return (
     <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-      <div className="">
+      <div className="card-container">
         <img
           src={character.image}
           alt="rick and morty character pic"
-          className="img-fluid my-3"
+          className="img-fluid my-3 card-image"
         />
-        <p className="character-name d-flex justify-content-center">
-          {character.name}
-        </p>
+        <p className="character-name">{character.name}</p>
       </div>
     </div>
   );
